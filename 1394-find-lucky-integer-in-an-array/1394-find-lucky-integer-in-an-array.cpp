7636d713e5bb810f675dad2d03333f43b90d7863
shivam0870 <96368173+shivam0870@users.noreply.github.com>
2022-05-21 00:14:39 +0530

    Time: 8 ms (59.74%), Space: 10.2 MB (59.87%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.cpp
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.cpp
@@ -13,19 +13,27 @@ public:
         // }
         // return ss;
         
-        int maxi=-1;
-        unordered_map<int,int> m;
-        for(int i=0;i<arr.size();i++)
-        {
-            m[arr[i]]++;
-        }
-        for(auto x:m)
-        {
-            if(x.second==x.first)
-                maxi = max(maxi, x.first);
+//         int maxi=-1;
+//         unordered_map<int,int> m;
+//         for(int i=0;i<arr.size();i++)
+//         {
+//             m[arr[i]]++;
+//         }
+//         for(auto x:m)
+//         {
+//             if(x.second==x.first)
+//                 maxi = max(maxi, x.first);
                 
-        }
-        return maxi;
+//         }
+//         return maxi;
+        
+         int ans=-1;
+        unordered_map<int, int>mp;
+        for(auto i: arr) mp[i]++;
+        for(auto itr=mp.begin();itr!=mp.end();itr++)
+            if(itr->first==itr->second && itr->first>ans)
+                ans=itr->first;
+        return ans;
         
     }
 };
