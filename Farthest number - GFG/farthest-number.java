4e32d0ee0f51ee0ee4af68415ba38f8f7f675273
shivam0870 <96368173+shivam0870@users.noreply.github.com>
2022-05-21 23:39:06 +0530

    Added solution - LeetHub

---
+++ Farthest number - GFG/farthest-number.java
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        PrintWriter out = new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(br.readLine()); 
+
+        while(t > 0){
+        	int N = Integer.parseInt(br.readLine()); 
+        	int[] Arr = new int[N];
+        	String line = br.readLine(); 
+            String[] strs = line.trim().split("\\s+"); 
+        	for(int i=0; i<N; i++)
+        	{
+        		Arr[i] = Integer.parseInt(strs[i]); 
+        	}
+        	
+
+            Solution ob = new Solution();
+			int[] ans = ob.farNumber(N,Arr);
+  
+            for(int i=0; i<ans.length; i++)
+                out.print(ans[i] + " ");
+            out.println();
+
+            t--;
+        }
+        out.flush();
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution 
+{ 
+    static int[] farNumber(int N, int Arr[])
+	{    
+	    
+       int disArr[]= new int[N];
+    for(int i=0;i<N;i++){
+        for(int j=N-1;j>=i;j--){
+            if(Arr[j]<Arr[i]){
+                disArr[i]=j;
+                break;
+            }
+            if(j==i){
+                disArr[i]=(-1);
+            }
+            continue;
+            
+        }
+    }
+    return disArr;  
+	}
+} 
