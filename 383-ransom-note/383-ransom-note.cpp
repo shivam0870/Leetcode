377f1982f899af60b2dd9c3fb332314c8a8c4fed
shivam0870 <96368173+shivam0870@users.noreply.github.com>
2022-05-21 23:26:41 +0530

    Time: 21 ms (54.54%), Space: 8.9 MB (14.40%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-         vector<int> vec(26, 0);
-        for (int i = 0; i < magazine.size(); ++i)
-             ++vec[magazine[i] - 'a'];
+        //  vector<int> vec(26, 0);
+        // for (int i = 0; i < magazine.size(); ++i)
+        //      ++vec[magazine[i] - 'a'];
+        // for (int j = 0; j < ransomNote.size(); ++j)
+        //     if (--vec[ransomNote[j] - 'a'] < 0)
+        //         return false;
+        // return true;
+        
+         unordered_map<char, int> map(26);
+         for (int i = 0; i < magazine.size(); ++i)
+            ++map[magazine[i]];
         for (int j = 0; j < ransomNote.size(); ++j)
-            if (--vec[ransomNote[j] - 'a'] < 0)
+            if (--map[ransomNote[j]] < 0)
                 return false;
         return true;
+        
+        
     }
 };
