b96010c135378812120da2c15e1d8f691252674e
shivam0870 <96368173+shivam0870@users.noreply.github.com>
2022-05-23 22:12:50 +0530

    Time: 2 ms (38.98%), Space: 5.9 MB (69.05%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     int firstBadVersion(int n) {
        int left = 0, right = n;
-        int ans = INT_MIN;
+        int ans =0;
         while(left<=right){
             int mid = left + (right - left) / 2;
             if(isBadVersion(mid)){
