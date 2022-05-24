4ce3f5d8c112c3c7dbb376f545d456f778bcf4f9
shivam0870 <96368173+shivam0870@users.noreply.github.com>
2022-05-25 00:22:04 +0530

    Added solution - LeetHub

---
+++ Maximum GCD of siblings of a binary tree - GFG/maximum-gcd-of-siblings-of-a-binary-tree.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int maxBinTreeGCD(vector<vector<int>> arr, int N) {
+        // code here
+        sort(arr.begin(), arr.end());
+        
+        int s = 0;
+    for(int i=0; i<N-2; i++){
+        if(arr[i][0] == arr[i+1][0])
+            s = max(s, __gcd(arr[i][1], arr[i+1][1]));
+    }
+    return s;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,u,v;
+        cin>>N;
+        
+        vector<vector<int>> arr;
+        
+        for(int i=0; i<N-1; i++)
+        {
+            cin>>u>>v;
+            vector<int> edge;
+            edge.push_back(u);
+            edge.push_back(v);
+            arr.push_back(edge);
+        }
+
+        Solution ob;
+        cout << ob.maxBinTreeGCD(arr,N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
