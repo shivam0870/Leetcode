da3a3a2dc044b1d1e9ae9cabd94a2ecf0ffb7fff
shivam0870 <96368173+shivam0870@users.noreply.github.com>
2022-06-13 11:46:53 +0530

    Time: 4 ms (89.10%), Space: 8.7 MB (70.75%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -4,10 +4,11 @@ public:
     
         
           vector<int> mini = triangle[triangle.size()-1];
-        for ( int i = triangle.size() - 2; i>= 0 ; i-- )
+        for ( int i = triangle.size()-2 ; i>= 0 ; i-- )
             for ( int j = 0; j < triangle[i].size() ;  j++ )
                 mini[j] = triangle[i][j] + min(mini[j],mini[j+1]);
         return mini[0];
+        
         
     }
 };
