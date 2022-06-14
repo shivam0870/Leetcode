d4cff303a9107e640be99b6af868764a00bc4782
shivam0870 <96368173+shivam0870@users.noreply.github.com>
2022-06-15 00:19:03 +0530

    Added solution - LeetHub

---
+++ Ishaan Loves Chocolates - GFG/ishaan-loves-chocolates.java
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		     
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    Complete obj = new Complete();
+		    int ans = obj.chocolates(arr, sizeOfArray);
+		    System.out.println(ans);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int chocolates (int arr[], int n) {
+    int min,i;
+   min=arr[0];
+   for(i=0;i<n;i++)
+   {
+       if(arr[i]<min)
+       {
+           min=arr[i];
+       }
+   }
+   return min;
+    }
+    
+    
+}
+
+
