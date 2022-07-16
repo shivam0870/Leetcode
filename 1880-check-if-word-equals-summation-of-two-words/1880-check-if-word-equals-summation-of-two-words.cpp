b4aed1c1e4f5a9d97ffb63d4821a0f6578a49a83
shivam0870 <96368173+shivam0870@users.noreply.github.com>
2022-07-16 09:51:53 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (10.46%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.cpp
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.cpp
@@ -1,15 +1,68 @@
 class Solution {
 public:
     
-    int getsum(string s){
-        int res = 0;
-        for(int i=0; i<s.size(); i++){
-            res = res * 10 + (s[i]-'a');
-        }
-        return res;
-    }
+//     int getsum(string s){
+//         int res = 0;
+//         for(int i=0; i<s.size(); i++){
+//             res = res * 10 + (s[i]-'a');
+//         }
+//         return res;
+//     }
+    
+    // bool isSumEqual(string firstWord, string secondWord, string targetWord) {
+    //     return getsum(firstWord) + getsum(secondWord) == getsum(targetWord);        
+    // }
     
     bool isSumEqual(string firstWord, string secondWord, string targetWord) {
-        return getsum(firstWord) + getsum(secondWord) == getsum(targetWord);        
+        string first,second,target;
+        /// converting first string to it's integer counterpart
+        for(auto child:firstWord){
+            int val=child-'a';
+            string temp=to_string(val);
+            first+=temp;
+        }
+        /// converting second string to it's integer counterpart
+        for(auto child:secondWord){
+            int val=child-'a';
+            string temp=to_string(val);
+            second+=temp;
+        }
+        /// converting third string to it's integer counterpart
+            for(auto child:targetWord){
+            int val=child-'a';
+            string temp=to_string(val);
+            target+=temp;
+        }
+       /// converting strings to integer using stoi function
+        int first_integer=stoi(first);
+        int second_integer=stoi(second);
+        int target_integer=stoi(target);
+        
+        return ((first_integer+second_integer)==target_integer);
     }
+    
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
