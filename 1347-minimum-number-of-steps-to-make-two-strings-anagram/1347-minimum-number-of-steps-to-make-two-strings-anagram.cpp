ea426390b4804b180ea419a5cb7788e281ceafb0
shivam0870 <96368173+shivam0870@users.noreply.github.com>
2022-07-19 16:20:23 +0530

    Time: 80 ms (70.22%), Space: 16.6 MB (75.37%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        unordered_map<char,int> mpp;
-        int sum = 0;
-        for(int i=0; i<s.size(); i++){
-            mpp[s[i]]++;
+        // unordered_map<char,int> mpp;
+        // int sum = 0;
+        // for(int i=0; i<s.size(); i++){
+        //     mpp[s[i]]++;
+        // }
+        // for(int i=0; i<s.size(); i++){
+        //     mpp[t[i]]--;
+        // }
+        // for(auto it : mpp){
+        //     if(it.second < 0){
+        //         sum = sum + it.second;
+        //     }
+        // }
+        // return abs(sum);
+        
+        
+         int Freq[26]={0};
+        int count=0;
+        for(int i=0;i<s.size();i++)
+        {
+            Freq[s[i]-'a']++;
+            Freq[t[i]-'a']--;
         }
-        for(int i=0; i<s.size(); i++){
-            mpp[t[i]]--;
+        
+        for(int i=0;i<26;i++)
+        {
+            if(Freq[i]>0)
+                count+=Freq[i];
         }
-        for(auto it : mpp){
-            if(it.second < 0){
-                sum = sum + it.second;
-            }
-        }
-        return abs(sum);
+        
+        return count;
+        
     }
 };
