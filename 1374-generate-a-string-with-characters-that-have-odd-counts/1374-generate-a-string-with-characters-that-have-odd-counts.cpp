023dc46d3d3c9ab69243718dafb00f233fe25b9d
Shivam kumar singh <96368173+shivam0870@users.noreply.github.com>
2022-08-03 01:10:24 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (67.34%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     string generateTheString(int n) {
-        if(n%2 != 0)
-           return string(n,'a');
-       return string(n-1,'a') + 'b';
+       //  if(n%2 != 0)
+       //     return string(n,'a');
+       // return string(n-1,'a') + 'b';
+        
+     string s;
+
+            if(n% 2 == 0)
+            {
+                for(int i = 0; i<n-1; ++i)
+                    s.push_back('a');
+                s.push_back('b');
+            }
+            else
+            {
+                for(int i = 0; i<n; ++i)
+                    s.push_back('a');
+            }
+           
+            return s;
+        
     }
 };
