105b6949f90737d1f319964f2962460fff2fd834
Shivam kumar singh <96368173+shivam0870@users.noreply.github.com>
2022-08-06 14:57:48 +0530

    Time: 11 ms (46.07%), Space: 9 MB (26.88%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -1,25 +1,54 @@
+// class Solution {
+// public:
+    
+//   static bool comp(const string &a, const string &b)
+//     {
+//         return a.size()<b.size();
+//     }
+//     vector<string> stringMatching(vector<string>& words) 
+//     {
+//         vector<string>v;
+//         sort(words.begin(),words.end(),comp);
+//         for(int i=0;i<words.size();i++)
+//         {
+//             for(int j=i+1;j<words.size();j++)
+//             {
+//                 if(words[j].find(words[i])!=-1)
+//                     {
+//                         v.push_back(words[i]);
+//                         break;
+//                     }
+//             }
+//         }
+//         return v;
+    
+//     }
+// };
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
-    
-  static bool comp(const string &a, const string &b)
-    {
-        return a.size()<b.size();
-    }
-    vector<string> stringMatching(vector<string>& words) 
-    {
-        vector<string>v;
-        sort(words.begin(),words.end(),comp);
-        for(int i=0;i<words.size();i++)
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+        for(auto i:words)
         {
-            for(int j=i+1;j<words.size();j++)
+            for(auto j: words)
             {
-                if(words[j].find(words[i])!=-1)
-                    {
-                        v.push_back(words[i]);
-                        break;
-                    }
+                if(i==j) continue;
+                if(j.find(i)!=-1)
+                {
+                    ans.push_back(i);
+                    break;                    
+                }
             }
         }
-        return v;
+        return ans;
     }
 };
