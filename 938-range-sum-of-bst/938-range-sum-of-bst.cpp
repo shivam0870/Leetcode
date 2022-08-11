5b9e5d2a68c2beb088f9c16353c1265514d50ecd
Shivam kumar singh <96368173+shivam0870@users.noreply.github.com>
2022-08-11 17:23:16 +0530

    Time: 343 ms (5.02%), Space: 69.3 MB (5.92%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -1,26 +1,66 @@
 class Solution {
 public:
-    int sum=0;
-    void findAns(TreeNode* root, int L, int R){
-       
-        if(root==NULL)
+   
+ int sum=0;
+    
+     void findAns(TreeNode* root, int L, int R){
+         
+    vector<int> tree;
+           if(root==NULL)
             return;
-        
             
         findAns(root->left,L,R);
-       
-        findAns(root->right,L,R);
-        if(root->val>=L and root->val<=R)
-        {
-            sum+=root->val;
+            findAns(root->right,L,R);
+       if(root->val>=L and root->val<=R){
+           tree.push_back(root->val);
+       }
+     
+         
+        for(int i=0; i<tree.size(); i++){
+            sum+=tree[i];
         }
-            
-        
-        
-    }
+         
+     }
+    
     int rangeSumBST(TreeNode* root, int L, int R) {
         findAns(root,L,R);
         return sum;
             
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     void findAns(TreeNode* root, int L, int R){
+       
+//         if(root==NULL)
+//             return;
+        
+            
+//         findAns(root->left,L,R);
+       
+//         findAns(root->right,L,R);
+//         if(root->val>=L and root->val<=R)
+//         {
+//             sum+=root->val;
+//         }
+            
+        
+        
+//     }
