c5e3137a0406c085222947ffe980f77e9d054e2b
Shivam kumar singh <96368173+shivam0870@users.noreply.github.com>
2022-08-18 16:26:12 +0530

    Time: 786 ms (5.01%), Space: 99.5 MB (5.44%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -3,7 +3,7 @@ public:
     int minSetSize(vector<int>& arr) {
         map<int,int> mpp;
         
-        //By default map orders by order of key and also in ascending order
+        // By default map orders by order of key and also in ascending order
         
         for(auto it : arr){
             mpp[it]++;
@@ -24,5 +24,60 @@ public:
             }
         }
         return cnt;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         map<int,int> mpp;
+//        for(auto it1 : nums){
+//            mpp[it1]++;
+//        }
+        
+        
+//         multimap<int,int,greater<int>> mp;
+//         for(auto it : mpp){
+//             mp.insert({it.second,it.first});
+//         }
+//         int cnt = 0;
+//         int n = nums.size();
+//         int sum = n;
+//         for(auto it = mp.begin(); it != mp.end(); it++){
+//             sum = sum - it->first;
+//             cnt++;
+//             if(cnt<=(n/2)){
+//                 return cnt;
+//             }
+//         }
+        
+//         return cnt;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
