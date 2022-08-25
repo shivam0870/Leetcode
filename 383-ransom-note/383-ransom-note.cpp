b860795dce57f43bca5350d09ac016232bc9f397
Shivam kumar singh <96368173+shivam0870@users.noreply.github.com>
2022-08-25 10:26:28 +0530

    Time: 21 ms (65.89%), Space: 9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -17,15 +17,24 @@ public:
         //         return false;
         // return true;
         
-        int count[26] = {0};
-        for(char ch : magazine)
-            count[ch - 'a']++;
+//         int count[26] = {0};
+//         for(char ch : magazine)
+//             count[ch - 'a']++;
         
-        for(char ch : ransomNote)
-            if(count[ch - 'a']-- <= 0)
+//         for(char ch : ransomNote)
+//             if(count[ch - 'a']-- <= 0)
+//                 return false;
+        
+//         return true;
+        unordered_map<char, int> map(26);
+        for (int i = 0; i < magazine.size(); ++i)
+            ++map[magazine[i]];
+        for (int j = 0; j < ransomNote.size(); ++j)
+            if (--map[ransomNote[j]] < 0)
                 return false;
-        
         return true;
+        
+        
         
     }
 };
