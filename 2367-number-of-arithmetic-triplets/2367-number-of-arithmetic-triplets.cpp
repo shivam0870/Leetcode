7b4604e2b3f1689a34d807326e0229cbf1a9ce2c
Shivam kumar singh <96368173+shivam0870@users.noreply.github.com>
2022-09-13 01:22:22 +0530

    Time: 12 ms (53.11%), Space: 9.1 MB (20.86%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,16 +1,47 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-         int ans=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                for(int k=j+1;k<nums.size();k++){
-                    if((nums[j]-nums[i])==diff && (nums[k]-nums[j])==diff){
-                        ans++;
-                    }
-                }
-            }
+        //  int ans=0;
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         for(int k=j+1;k<nums.size();k++){
+        //             if((nums[j]-nums[i])==diff && (nums[k]-nums[j])==diff){
+        //                 ans++;
+        //             }
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        
+        
+        
+        
+        
+        
+        int cnt = 0;
+        unordered_map<int,bool> mpp;
+        for(int i=0; i<nums.size(); i++){
+            mpp[nums[i]] = true;
         }
-        return ans; 
+        
+        for(int i=0; i<nums.size(); i++){
+            if(mpp[nums[i]-diff] and mpp[nums[i] + diff])
+                cnt++;
+        }
+        
+        return cnt;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
