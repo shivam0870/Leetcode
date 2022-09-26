fefa27ca2f18c275cd0cbd4b339cf380017729ef
Shivam kumar singh <96368173+shivam0870@users.noreply.github.com>
2022-09-26 12:27:39 +0530

    Time: 8 ms (24.46%), Space: 8.4 MB (35.52%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -2,19 +2,26 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
        
-        sort(nums.begin() , nums.end());
+//         sort(nums.begin() , nums.end());
        
-        int cnt =0;
+//         int cnt =0;
+//         for(int i=0; i<nums.size(); i++){
+//             if(nums[i] > 0){
+//             cnt++;
+//            int x = nums[i];
+//             for(int j=i; j<nums.size(); j++){
+//                 nums[j]-=x;
+//             }
+//             }
+//         }
+//         return cnt;
+        
+        set<int> s;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] > 0){
-            cnt++;
-           int x = nums[i];
-            for(int j=i; j<nums.size(); j++){
-                nums[j]-=x;
-            }
-            }
+            if(nums[i] != 0)
+            s.insert(nums[i]);
         }
-        return cnt;
+        return s.size();
         
     }
 };
