ab3d1d7d1d91cbb555810050fb2453b021118c2b
Shivam kumar singh <96368173+shivam0870@users.noreply.github.com>
2022-11-14 19:12:27 +0530

    Time: 11 ms (32.47%), Space: 11 MB (75.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,76 @@
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//       int l=0,r=nums.size()-1;
+//         while(l<=r){
+//             int mid = (l+r)/2;
+//             if(nums[mid] == target)
+//                 return mid;
+//             //Either the left side will be sorted
+//             if(nums[l] <= nums[mid]){
+//                 //Figure out that if the element lies in the left half or not
+//                 if(target>=nums[l] and target<=nums[mid]){
+//                     r  = mid -1;
+//                 }
+//                 else{
+//                     l = mid + 1;
+//                 }
+//             }
+//             //Either the right half is sorted
+//            else{
+//                if(target >= nums[mid] and target <= nums[r]){
+//                    l=mid+1;
+//                }
+//                else{
+//                    r = mid-1;
+//                }
+//            }
+//         }
+//         return -1;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-      int l=0,r=nums.size()-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(nums[mid] == target)
-                return mid;
-            //Either the left side will be sorted
-            if(nums[l] <= nums[mid]){
-                //Figure out that if the element lies in the left half or not
-                if(target>=nums[l] and target<=nums[mid]){
-                    r  = mid -1;
-                }
-                else{
-                    l = mid + 1;
-                }
-            }
-            //Either the right half is sorted
-           else{
-               if(target >= nums[mid] and target <= nums[r]){
-                   l=mid+1;
-               }
-               else{
-                   r = mid-1;
-               }
-           }
+   int i=0,j=nums.size()-1;
+        while(i<=j){
+            if(nums[i] == target)
+                return i;
+            if(nums[j] == target)
+                return j;
+            i++;
+            j--;
         }
         return -1;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
